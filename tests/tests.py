ddf644aae0cef4a2b0eea6c34e93c55428483bea
chayko-anna <al.nchko@gmail.com>
2025-02-07 18:19:02 +0300

    fix

--- tests/tests.py
+++ tests/tests.py
@@ -1,41 +1,41 @@
 import allure
-from selene import have, by
+from selene import have, browser
+import os
 
 
 @allure.title("Successful fill form")
 def test_successful(browser_settings):
-    browser = browser_settings
-    first_name = "Anna"
-    last_name = "Chayko"
+    with allure.step("Открыть страницу с формой"):
+        browser.open('https://demoqa.com/automation-practice-form')
 
-    with allure.step("Open registrations form"):
-        browser.open("https://demoqa.com/automation-practice-form")
-        browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
-        browser.driver.execute_script("$('footer').remove()")
-        browser.driver.execute_script("$('#fixedban').remove()")
+    with allure.step("Заполнить имя и фамилию"):
+        browser.element('[id="firstName"]').type('Anna')
+        browser.element('[id="lastName"]').type('Chayko')
 
-    with allure.step("Fill form"):
-        browser.element("#firstName").set_value(first_name)
-        browser.element("#lastName").set_value(last_name)
-        browser.element("#userEmail").set_value("al.nchko@gmail.com")
-        browser.element("#genterWrapper").element(by.text("Female")).click()
-        browser.element("#userNumber").set_value("1231231231")
-        # browser.element("#dateOfBirthInput").click()
-        # browser.element(".react-datepicker__month-select").s("13")
-        # browser.element(".react-datepicker__year-select").selectOption("2000")
-        # browser.element(".react-datepicker__day--013:not(.react-datepicker__day--outside-month)").click()
-        browser.element("#subjectsInput").send_keys("Arts")
-        browser.element("#subjectsInput").press_enter()
-        browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
-        # browser.element("#uploadPicture").uploadFromClasspath("img/1.png")
-        browser.element("#currentAddress").set_value("Улица Пушкина, дом Колотушкина")
-        browser.element("#state").click()
-        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
-        browser.element("#city").click()
-        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
-        browser.element("#submit").click()
+    with allure.step("Заполнить email"):
+        browser.element('[id="userEmail"]').type('al.nchko@gmail.com')
 
-    with allure.step("Check form results"):
-        browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
-        # browser.element(".table-responsive").should(
-        #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
+    with allure.step("Выбрать пол"):
+        browser.element('[for="gender-radio-2"]').click()
+
+    with allure.step("Заполнить номер телефона"):
+        browser.element('[id="userNumber"]').type('9102906632').press_tab()
+
+    with allure.step("Выбрать дату рождения"):
+        browser.element('[class="react-datepicker__month-select"]').element('[value="0"]').click()
+        browser.element('[class="react-datepicker__year-select"]').element('[value="2000"]').click()
+        browser.element('[class="react-datepicker__day react-datepicker__day--013"]').click()
+
+    with allure.step("Выбрать предмет"):
+        browser.element('[id="subjectsInput"]').type('A')
+        browser.all('.subjects-auto-complete__menu').element_by(have.text('Arts')).click()
+
+    with allure.step("Выбрать хобби"):
+        browser.element('[for="hobbies-checkbox-1"]').click()
+        browser.element('[for="hobbies-checkbox-2"]').click()
+
+    with allure.step("Загрузить изображение"):
+        browser.element('[id="uploadPicture"]').set_value(os.path.abspath("../tests/shutterstock_2331893385.jpg"))
+
+    with allure.step("Заполнить адрес"):
+        browser.element('[id="currentAddress"]').type('Улица Пушкина, дом Колотушкина')
